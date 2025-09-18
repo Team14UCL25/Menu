839c08ae39133eeace6b74b3a76eda042f965979
Jeppe Helsted <jeppe@helsted.com>
2025-09-18 09:38:06 +0200

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace MenuSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
